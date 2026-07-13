0f13374f731da1b957a27f847241bcb85d36aa4d
yk <lei.wang09@sap.com>
2026-07-13 18:35:29 +0800

    Update mermaid-code.rb

--- Casks/mermaid-code.rb
+++ Casks/mermaid-code.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 cask "mermaid-code" do
-  arch arm: "aarch64", intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  arch arm: "aarch64", intel: "x64"
 
   version "0.3.4"
   sha256 arm:   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
