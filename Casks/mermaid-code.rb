3826b526dc631ee92dd26af4f584a90c96524ac0
yk <lei.wang09@sap.com>
2026-07-14 17:51:07 +0800

    Revert mermaid-code.rb

--- Casks/mermaid-code.rb
+++ Casks/mermaid-code.rb
@@ -4,9 +4,9 @@
 cask "mermaid-code" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.3.5"
-  sha256 arm:   "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5",
-         intel: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.3.4"
+  sha256 arm:   "5cf8d91c130dcc78cbbf60d90656ee61d5466e101ac435653ad3605d46685a6f",
+         intel: "496dbf5ccca924f0f197c2ed9dbdf8e51fa3b0fa83c9721b75553d0aeb4a2dd2"
 
   url "https://github.com/m8524769/mermaid-code/releases/download/v#{version}/Mermaid.Code_#{version}_#{arch}.dmg"
   name "Mermaid Code"
