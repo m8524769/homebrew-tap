3c70d75477ca60d91c2307d5341ea6e3a980fbea
GitHub Action <action@github.com>
2026-09-09 16:57:49 +0000

    Update mermaid-code to 0.7.3

--- Casks/mermaid-code.rb
+++ Casks/mermaid-code.rb
@@ -4,9 +4,9 @@
 cask "mermaid-code" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.7.2"
-  sha256 arm:   "5185a9a31b076994f815e24c6d73ef0f727c9e285b14685a7887554ca9f90ce4",
-         intel: "f6699585443e20ff27284f3a170bed14bb8e68a1dfa2e38b999b76a8fdbd1dd4"
+  version "0.7.3"
+  sha256 arm:   "ff49fb0294beff571aa4ef7b9e4a96951c3cd49d04493e3789c4efc70635bb78",
+         intel: "4f20572b75450f63604e46fce9a8dcd0b0be033827d6d72bed6c12ab055f0023"
 
   url "https://github.com/m8524769/mermaid-code/releases/download/v#{version}/Mermaid.Code_#{version}_#{arch}.dmg"
   name "Mermaid Code"
